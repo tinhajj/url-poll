f191cc7c2f079d0cce38b35f57d66ab173c45ba3
Thanoj Singh <thanoj.singh@gmail.com>
2017-08-23 00:24:04 -0400

    add request and formatted print

--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/andrew-d/go-termutil"
+	"net/http"
 	"os"
 	"sync"
 	"time"
 )
 
-var wg sync.WaitGroup
+var wg sync.WaitGroup //waitgroup for polling results
+var results chan string
 
 func main() {
+	start := time.Now()
+
 	scanner := bufio.NewScanner(os.Stdin)
+	results = make(chan string)
 
 	if !(len(os.Args) > 1) && termutil.Isatty(os.Stdin.Fd()) {
 		fmt.Println("No arguments given and nothing to read from stdin")
@@ -33,11 +38,26 @@ func main() {
 		}
 	}
 
-	wg.Wait()
-	time.Sleep(1000 * time.Millisecond)
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for info := range results {
+		fmt.Println(info)
+	}
+
+	fmt.Println(time.Since(start))
 }
 
 func poll(url string) {
 	defer wg.Done()
-	fmt.Println(url)
+	resp, err := http.Get(url)
+
+	if err != nil {
+		results <- err.Error()
+		return
+	}
+
+	results <- fmt.Sprintf("%s\t%s", resp.Status, url)
 }
